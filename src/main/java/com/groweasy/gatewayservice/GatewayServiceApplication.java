1dcd63b6381c714db4b803e7abdd8747098f4644
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2024-06-08 21:38:18 -0500

    feature: cors configurer

--- src/main/java/com/groweasy/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/groweasy/gatewayservice/GatewayServiceApplication.java
@@ -1,10 +1,31 @@
 package com.groweasy.gatewayservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @SpringBootApplication
 public class GatewayServiceApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Permitir los métodos necesarios
+						.allowedHeaders("*"); // Permitir todos los encabezados
+			}
+
+		};
+	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
